8096f635f5ac30708a197a8a5c6bb0fbb63541e0
rakib <hassanrakibul926@gmail.com>
2023-12-20 12:20:14 +0600

    add user complate

--- src/Pages/Dashbord/Dashbord.jsx
+++ src/Pages/Dashbord/Dashbord.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import { FaRegPlusSquare, FaSearch } from "react-icons/fa"
-import Swal from "sweetalert2";
+import { FaRegPlusSquare, FaSearch } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Dashbord = () => {
+const Dashboard = () => {
 
-    const [isModalOpen, setModalOpen] = useState(false)
-
-    const handleAddUser = async (event) =>{
-        event.preventDefault()
+    const handleAddUser = async (event) => {
+        event.preventDefault();
         const form = event.target;
         const name = form.full_name.value;
         const email = form.email.value;
@@ -17,52 +16,45 @@ const Dashbord = () => {
             name,
             email,
             phone,
-            createAT: new Date(),
-            updateAt: new Date()
-        }
-        
-        console.log(userData)
+            createAt: new Date(),
+            updateAt: new Date(),
+        };
 
-        try{
+        try {
             const response = await fetch('http://localhost:5000/user', {
-                method:'POST',
+                method: 'POST',
                 headers: {
-                    'Content-Type':'application/json'
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(userData)
+                body: JSON.stringify(userData),
             });
-            if(!response.ok){
-                console.log("Netwok request faild")
+
+            if (!response.ok) {
+                console.log("Network request failed");
+            } else {
+                // Handle successful response
+                toast.success("User successfully added to the database!");
+                form.reset()
+                // Swal.fire({
+                //     title: "User Successfully Added",
+                //     text: "User successfully added to the database",
+                //     icon: "success",
+                // });
             }
-            setModalOpen(false)
-
-            Swal.fire({
-                title: "User SUccessfully Added",
-                text: "User Successfully added in database",
-                icon: "success"
-              });
-            
-        }catch(error) {
-            console.log(error)
+        } catch (error) {
+            console.error("Error adding user:", error.message);
+            // Handle error, e.g., show an error message.
         }
-
-        fetch('http://localhost:5000/user', userData,{
-            method: "POST",
-            type: {
-                'content-type':'json/ontent'
-            },
-            body: JSON.body()
-        })
-
-    }
+    };
 
     return (
         <div className="bg-base-200 mt-6 md:mt-16 p-4">
+            <ToastContainer />
             <div className="flex justify-between">
                 <div className="">
                     <form className="">
                         <input type="text" placeholder="Search" className="border-2 p-2 w-[20rem] relative rounded-l-xl outline-none focus:border focus:border-primaryColor" />
-                        <button className="border-y-2 border-r-2 py-3 px-6 rounded-r-xl bg-white absolute hover:bg-base-200"><FaSearch></FaSearch></button>
+                        <button className="border-y-2 border-r-2 py-3 px-6 rounded-r-xl bg-white absolute hover:bg-base-200"><FaSearch /></button>
                     </form>
                 </div>
                 <div className="">
@@ -80,12 +72,12 @@ const Dashbord = () => {
             </div>
             <div>
                 <div className="w-3/12 h-32 flex justify-center items-center border border-primaryColor rounded-md">
-                    <FaRegPlusSquare onClick={() => setModalOpen(true)} className="text-[56px] hover:text-primaryColor"></FaRegPlusSquare>
+                    <FaRegPlusSquare onClick={() => document.getElementById('my_modal_1').showModal()} className="text-[56px] hover:text-primaryColor" />
                 </div>
             </div>
 
             {/* add user modal  */}
-            <dialog id="my_modal_1" className={`modal ${isModalOpen ? 'open':''}`}>
+            <dialog id="my_modal_1" className="modal">
                 <div className="modal-box">
                     <h3 className="font-bold text-lg text-center">Add User</h3>
                     <div>
@@ -116,13 +108,13 @@ const Dashbord = () => {
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button in form, it will close the modal */}
-                            <button className="btn">Close</button>
+                            <button className="btn" onClick={() => setModalOpen(false)}>Close</button>
                         </form>
                     </div>
                 </div>
             </dialog>
         </div>
-    )
+    );
 }
 
-export default Dashbord
+export default Dashboard;
